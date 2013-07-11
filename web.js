4ab38909ab0e52982710c35c49cef94d53597857
paling82 <pieter@baecke.net>
2013-07-11 14:47:26 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,16 +3,13 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync('index.html', function(err, data){
-    if(err) throw err;
-    console.log(data);
-});
+var buffer = fs.readFileSync('index.html', null);
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8"));
+  response.send("lala");
 });
 
-var port = process.env.PORT || 5000;
+/*var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
